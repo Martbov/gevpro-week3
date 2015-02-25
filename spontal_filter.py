9bbb0b7304403dc7aed82bc081640894b75c7f55
Martbov <Mart@RN-145-97-167-20.eduroam.rug.nl>
2015-02-25 11:38:35 +0100

    Added blood-die.json

--- spontal_filter.py
+++ spontal_filter.py
@@ -11,20 +11,19 @@ def main(argv):
 	tree = ET.parse(spontalXML)
 	tree.write(spontalFilter, encoding="unicode", xml_declaration=True)
 	spontalXML.close()
-	filterTree = ET.parse(spontalFilter)
-	root = filterTree.getroot()
 	
-	for point in root.findall('POINT'):
-		f0_start = point.find('F0_START').text
-		f0_end = point.find('F0_END').text
-		bottom_hz = point.find('BOTTOM_HZ').text
-		top_hz = point.find('TOP_HZ').text
+	root = tree.getroot()
+	
+	for point in root.iter('POINT'):
+		f0_start = float(point.find('F0_START').text)
+		f0_end = float(point.find('F0_END').text)
+		bottom_hz = float(point.find('BOTTOM_HZ').text)
+		top_hz = float(point.find('TOP_HZ').text)
 		print(f0_start, f0_end, bottom_hz, top_hz)
 		
-		#if bottom_hz <= f0_start and f0_end <= top_hz:
+		if not bottom_hz <= f0_start <= top_hz and bottom_hz <= f0_end <= top_hz:
+			root.remove(point)
 
-		
-			
 
 if __name__ == '__main__':
 	main(sys.argv)
