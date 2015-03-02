4801662a323523ca445cedfdaf35db6dd6f14c02
Martbov <Mart@RN-145-97-165-58.eduroam.rug.nl>
2015-03-02 13:03:23 +0100

    Completed spontal_filter.py

--- spontal_filter.py
+++ spontal_filter.py
@@ -7,23 +7,22 @@ import xml.etree.ElementTree as ET
 
 def main(argv):
 	spontalXML = open((argv[1]), 'r')
-	spontalFilter = open('spontal_filtered.xml', 'w')
 	tree = ET.parse(spontalXML)
-	tree.write(spontalFilter, encoding="unicode", xml_declaration=True)
 	spontalXML.close()
 	
 	root = tree.getroot()
 	
-	for point in root.iter('POINT'):
+	for point in root:
 		f0_start = float(point.find('F0_START').text)
 		f0_end = float(point.find('F0_END').text)
 		bottom_hz = float(point.find('BOTTOM_HZ').text)
 		top_hz = float(point.find('TOP_HZ').text)
 		print(f0_start, f0_end, bottom_hz, top_hz)
 		
-		if not bottom_hz <= f0_start <= top_hz and bottom_hz <= f0_end <= top_hz:
+		if not bottom_hz <= f0_start <= top_hz or not bottom_hz <= f0_end <= top_hz:
 			root.remove(point)
 
+	tree.write('spontalFilter.xml')
 
 if __name__ == '__main__':
 	main(sys.argv)
